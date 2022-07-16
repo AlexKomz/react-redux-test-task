927d1258f8cb39cdf4c1a241d2a9b81d8bbeef3c
Aleksandr <60098929+AlexKomz@users.noreply.github.com>
2022-07-16 15:35:44 +0300

    Added some file structure

---
+++ src/components/Game/components/Area/Area.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Area: React.FC = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
---
+++ src/layouts/Base/Base.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type BaseProps = {
+  children: React.ReactNode;
+};
+
+export const Base: React.FC<BaseProps> = ({ children }) => {
+  return (
+    <div>
+
+    </div>
+  );
+};
