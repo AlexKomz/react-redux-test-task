7a61dbea3f0fe5e0655f2644da49faf96c306edd
Aleksandr <60098929+AlexKomz@users.noreply.github.com>
2022-07-16 15:42:22 +0300

    Added base centred layout

--- src/layouts/Base/Base.tsx
+++ src/layouts/Base/Base.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import styled from 'styled-components'
 
 type BaseProps = {
   children: React.ReactNode;
 };
 
+const CentredWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 export const Base: React.FC<BaseProps> = ({ children }) => {
   return (
-    <div>
-
-    </div>
+    <CentredWrapper>
+      { children }
+    </CentredWrapper>
   );
 };
